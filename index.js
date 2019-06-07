96953465b540496ce1589367dfe9dbacacbaa210
Abhishek Paresh Jajal <abhijajal@gmail.com>
2019-06-06 20:28:05 -0500

    update

---
+++ index.js
@@ -0,0 +1,58 @@
+$(function() {
+  var firebaseDb = firebase.database();
+  
+  firebaseDb.ref('/personalInfo').once('value').then(function(snapshot) {
+  $('#name').text(snapshot.val().name);
+  $('#tagLine').text(snapshot.val().tagLine);
+  $('#aboutMe').text(snapshot.val().aboutMe);
+
+  // ...
+});
+
+  var skillsRef = firebaseDb.ref('/skills');
+skillsRef.on('child_added', function(data) {
+	$('#skillList').append("<li>● "+data.val()+"</li>")
+});
+
+
+firebaseDb.ref('/resumeUrl').once('value').then(function(snapshot) {
+	$('#resumeUrl'). attr("href", snapshot.val()); 
+});
+ 
+
+
+firebaseDb.ref('/contact').once('value').then(function(snapshot) {
+	$('#address').append(snapshot.val().address)
+	$('#phone').append(snapshot.val().phone)
+	$('#email').append(snapshot.val().email)
+	$('#facebook').attr("href", snapshot.val().facebook);
+	$('#linkedIn').attr("href", snapshot.val().linkedIn);
+	$('#formAction').attr("action", snapshot.val().formAction);
+});
+ 
+
+
+ var projectsRef = firebaseDb.ref('/projects');
+projectsRef.on('child_added', function(data) {
+$("#projects").append(
+
+  '<div class="w3-card-4" style="width:100%;margin-bottom:25px">'
+  +'<header class="w3-container w3-light-grey">'
+  +'    <h3>'+data.val().name+'</h3>'
+  +'  </header>'
+  +'  <div class="w3-container" style="background: gray">'
+  +'    <hr>'
+  +'    <img src="'+data.val().logo+'" alt="Avatar" class="w3-left w3-margin-right" style="width:100px">'
+  +'    <p style="color: white">'+data.val().desc+'</p><br>'
+  +'  </div>'
+  +'<form action="projectDetails.html" method="get">'
+  +'<input type="text" style="display:none" name="projectId" value="'+data.key+'">'
+  +'<input type="submit" class="w3-button w3-block w3-dark-grey" value="See More">'
+  +'</form>'
+  +'</div>');
+
+});
+ 
+
+  
+});
---
+++ projectDetails.js
@@ -0,0 +1,36 @@
+$(function() {
+  var firebaseDb = firebase.database();
+  $.urlParam = function(name){
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	return results[1] || 0;
+}
+
+var projectId=$.urlParam('projectId'); 
+var projectsRef = firebaseDb.ref('/projects/'+projectId);
+var type;
+
+projectsRef.once('value').then(function(snapshot) {
+  $('#name').text(snapshot.val().name);
+  $('#desc').text(snapshot.val().desc);
+  type=snapshot.val().type;
+  $('#logo').attr("src", snapshot.val().logo);
+});
+
+var snapRef = firebaseDb.ref('/projects/'+projectId+'/snaps');
+snapRef.on('child_added', function(data) {
+	if(type=="mob")
+	{
+		$('#web').append('<img src="'+data.val()+'" style="width: 275px">')	
+	}
+	else
+	{
+		$('#web').append('<img src="'+data.val()+'" style="width: 100%">')
+	}
+	$('#mob').append('<img src="'+data.val()+'" style="width: 100%">')
+		
+});
+ 
+
+
+});
+
